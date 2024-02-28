e6a13a04861d8e716deaba32fdc956c6ebe3a283
Anna Keesman <anna.keesman@ordina.nl>
2024-02-28 11:51:28 +0100

    Updates

--- cypress/e2e/Webdriver/contact-us.js
+++ cypress/e2e/Webdriver/contact-us.js
@@ -4,6 +4,12 @@ describe("Test Contact Us Form via Webdriver ", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    
+     cy.fixture('user-data').then(function(data) {
+        //thisdata = data    (this might not work)
+        globalThis.data = data
+        
+    })
   });
 
   it("Should be able to submit a successful sumission via contact form us form ", () => {
@@ -11,10 +17,10 @@ describe("Test Contact Us Form via Webdriver ", () => {
     cy.title().should("include", "WebDriver | Contact Us");
     cy.url().should("include", "contactus");
 
-    cy.get('[name="first_name"]').type("Anduin");
-    cy.get('[name="last_name"]').type("Wrynn");
-    cy.get('[name="email"]').type("anduin.wrynn@azeroth.com");
-    cy.get("textarea.feedback-input").type("May be light be with u");
+    cy.get('[name="first_name"]').type(data.name);
+    cy.get('[name="last_name"]').type(data.lastname);
+    cy.get('[name="email"]').type(data.email);
+    cy.get("textarea.feedback-input").type(data.comment);
     cy.get('[type="submit"]').click();
     cy.get("h1").should("have.text", "Thank You for your Message!");
   });
