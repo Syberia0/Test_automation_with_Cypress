fbf68185cfbd03382a102232582f60196d42b5d5
Anna Keesman <anna.keesman@ordina.nl>
2024-02-29 13:17:12 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require("cypress");
 
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress\\config', `${file}.json`)
+
+  if(!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found.")
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile)
+}
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
