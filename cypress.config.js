67135ca50aa95cb3ce6bfc155b1ba9bd0dce52ec
Anna Keesman <anna.keesman@ordina.nl>
2024-03-01 11:05:25 +0100

    projectID

--- cypress.config.js
+++ cypress.config.js
@@ -15,6 +15,7 @@ function getConfigurationByFile(file) {
 }
 
 module.exports = defineConfig({
+  projectId: '2yydp7',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -25,8 +26,18 @@ module.exports = defineConfig({
     chromeWebSecurity: false,
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 120000,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    video: false,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json',
+    },
     env: {
-      webdriveruni_homepage: "http://www.webdriveruniversity.com/"
+      webdriveruni_homepage: "http://www.webdriveruniversity.com/",
+      automation_test_store: "https://automationteststore.com/"
     }
     
     //baseUrlStore: "https://automationteststore.com/"
