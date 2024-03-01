e55ec6e7d776872689f25e183af1c0e1196cb84a
Anna Keesman <anna.keesman@ordina.nl>
2024-03-01 18:26:50 +0100

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -19,6 +19,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const file = config.env.configFile || 'development'
+      return getConfigurationByFile(file)
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     excludeSpecPattern: "cypress/e2e/other/*js",
@@ -34,6 +36,10 @@ module.exports = defineConfig({
     reporter: 'cypress-multi-reporters',
     reporterOptions: {
       configFile: 'reporter-config.json',
+    },
+    retries: {
+      runMode: 0,
+      openMode: 0,
     },
     env: {
       webdriveruni_homepage: "http://www.webdriveruniversity.com/",
